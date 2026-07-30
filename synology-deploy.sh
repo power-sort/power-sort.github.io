455f449271bba3b51da4e77c4b9e2bfea3c6a7cc
Sebastian Wild <wild@liverpool.ac.uk>
2026-07-30 18:18:17 +0200

    sync linkls

--- synology-deploy.sh
+++ synology-deploy.sh
@@ -1,1 +1,1 @@
-rsync -e 'ssh -p 222' -av --delete ./ Sebastian@disk.wild-inter.net:/volume1/web/powersort/ --exclude=.git --exclude=.gitignore --exclude /synology-deploy.sh --exclude powersort-site.zip --exclude powersort-site-v3.zip
+rsync -e 'ssh -p 222' -av --copy-links --delete ./ Sebastian@disk.wild-inter.net:/volume1/web/powersort/ --exclude=.git --exclude=.gitignore --exclude /synology-deploy.sh --exclude powersort-site.zip --exclude powersort-site-v3.zip
