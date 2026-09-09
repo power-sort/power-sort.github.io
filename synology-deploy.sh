0abb975d24dfa3730b1e3573ca5e47b14ac5acce
Sebastian Wild <wild@liverpool.ac.uk>
2026-09-09 19:34:00 +0200

    public leaderboard, level progress

--- synology-deploy.sh
+++ synology-deploy.sh
@@ -1,5 +1,5 @@
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
 chmod +x synology-deploy.sh
-rsync -e 'ssh -p 222' -av --copy-links --delete ./ Sebastian@disk.wild-inter.net:/volume1/web/powersort/ --exclude=.git --exclude=.gitignore --exclude /synology-deploy.sh --exclude powersort-site.zip --exclude powersort-site-v3.zip
+rsync -e 'ssh -p 222' -av --copy-links --delete ./ Sebastian@disk.wild-inter.net:/volume1/web/powersort/ --exclude=.git --exclude=.gitignore --exclude /synology-deploy.sh --exclude \*.zip --exclude powersorts-pursuit-v\* --exclude playground
 ssh -p222 Sebastian@disk.wild-inter.net 'chmod -R go+r /volume1/web/*'
